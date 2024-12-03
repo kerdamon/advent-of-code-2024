93db50e8f575ffdfb5f376f89d15865c66fb1d3c
kerdamon <kwalas314@gmail.com>
2024-12-03 16:42:12 +0100

    Add template for solutions

---
+++ src/template/solution_part_1.py
@@ -0,0 +1,13 @@
+from common import parse_input_lines
+
+def parse_input(input_lines: list[str]):
+  pass
+
+def solve(input) -> int:
+  pass
+
+if __name__ == '__main__':
+  input_lines = parse_input_lines()
+  input = parse_input(input_lines)
+  result = solve(input)
+  print(f'Result: {result}')
